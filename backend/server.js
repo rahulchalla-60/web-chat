6eebed01cfe05d27980f5b160a22c1ca8a610ab0
rahul_challa <rahulchalla.a@gmail.com>
2025-05-26 13:54:58 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config();
+connectDB();
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>console.log(`server is running on port ${PORT}`));
